347b311ed602e82f9180fcc354d21a2de07edf79
qqinxl <qqinxl@gmail.com>
2012-10-03 18:41:37 +0900

    add blog

---
+++ 02/blog-web-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogWebApp::Application.config.secret_token = '760599fefc7c35960fbe0230c2b988163e50808c74b29b517652d63b8c1b45f9cc37eb4d982b0998c6b5c3c5683b279110eff0e657c53b6c6f29ab46567ff2c6'
---
+++ 02/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '231bd7e1c1c6ff3f7db979016154594ab186614cd50db6195902a9967c2dcfc1453e8404a6370ce69505d8d717804bb066329b39719e9d4ecd3e4cf7c3cd0ad3'
